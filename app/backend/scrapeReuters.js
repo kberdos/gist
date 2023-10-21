f21e6d4bae9febbd545e3460d9e8a2e8109b95aa
Tanish Makadia <tanishmakadia@gmail.com>
2023-10-21 16:16:28 -0400

    feat: Scrape entire world category from Reuters

--- app/backend/scrapeReuters.js
+++ app/backend/scrapeReuters.js
@@ -8,18 +8,42 @@ const scrapeCategory = async (category) => {
 
     let articles = [];
     
-    $("li.story-collection__story__LeZ29").each((i, element) => {
-        articles.push({
-            title: $(element).find("h3").find("a").text(),
-            summary: "",
-            url: $(element).find("h3").find("a").attr("href"),
-            source: "Reuters"
-        });
-    });
+    // scrape article titles and urls
+    const elements = $("li.story-collection__story__LeZ29");
 
-    console.log(articles);
+    for (let i = 0; i < elements.length; i++) {
+        try {
+            // extract article title and url
+            let items = {
+                title: $(elements[i]).find("h3").find("a").text(),
+                summary: "",
+                url: $(elements[i]).find("h3").find("a").attr("href"),
+                source: "Reuters"
+            };
+
+            // update summary field
+            items.summary = await scrapeArticle(`https://www.reuters.com${items.url}`);
+        
+            articles.push(items);
+        } catch (err) {
+            continue;
+        }
+    }
 
     return articles;
+}
+
+const scrapeArticle = async (url) => {
+    const html = await axios.get(url);
+    const $ = await cheerio.load(html.data);
+
+    let articleText = [];
+
+    $("p.text__text__1FZLe").each((i, element) => {
+        articleText.push($(element).text());
+    });
+
+    return articleText.join(" ");
 }
 
 console.log(await scrapeCategory("world"));
