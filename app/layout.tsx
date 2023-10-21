4308c339f4ee86972e49e7037f6124259c5e514d
kberdos <kazuya_erdos@brown.edu>
2023-10-21 11:57:24 -0400

    gist

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Fira_Sans } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
+const fira = Fira_Sans({ weight: "600", subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={fira.className}>{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="text-3xl text-center">Gist</div>
+    <div className="bg-nord-0 h-screen">
+      <div className="pt-[100px] text-white text-7xl text-center">Gist</div>
+      <div className="flex flex-col">
+        <div className="bg-white rounded-full mt-8 py-5  text-nord-1 text-2xl mx-[250px] px-10 font-normal text-center ">Give me the gist about...</div>
+        <div className="ml-auto mr-[285px] w-0 h-0 border-t-transparent border-r-[50px] border-r-white border-b-[30px] border-b-transparent">
+        </div>
+      </div>
+    </div>
   )
 }
