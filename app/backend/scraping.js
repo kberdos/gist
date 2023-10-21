4092d3a63f27dd2561d027ab0f9a515cfbcbc8bb
Tanish Makadia <tanishmakadia@gmail.com>
2023-10-21 12:08:10 -0400

    feat: Implement scraping for all NYTimes news categories

--- app/backend/scraping.js
+++ app/backend/scraping.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import cheerio from "cheerio";
 
-const scraper = async () => {
-    const  url = "https://www.nytimes.com/section/world";
+const scrapeNYTimes = async (category) => {
+    const  url = `https://www.nytimes.com/section/${category}`;
     const html = await axios.get(url);
     const $ = await cheerio.load(html.data);
 
@@ -13,10 +13,30 @@ const scraper = async () => {
             title: $(element).find("h3").text(),
             summary: $(element).find("p").text(),
             url: $(element).find("a").attr("href"),
+            source: "New York Times"
         })
     });
 
-    console.log(articles);
+    return articles;
 }
 
-scraper();
+let data = {
+    world: await scrapeNYTimes("world"),
+    us: await scrapeNYTimes("us"),
+    politics: await scrapeNYTimes("politics"),
+    science: await scrapeNYTimes("science"),
+    sports: await scrapeNYTimes("sports"),
+    health: await scrapeNYTimes("health"),
+    // ny: [],
+    // business: [],
+    // opinion: [],
+    // arts: [],
+    // books: [],
+    // style: [],
+    // food: [],
+    // travel: [],
+    // magazine: [],
+    // realEstate: [],
+};
+
+console.log(data);
