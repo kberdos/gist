2126590904b1d21a9ef2c2b28aed0d2d8b7e731c
Tanish Makadia <tanishmakadia@gmail.com>
2023-10-21 17:37:40 -0400

    perf: Make NYTimes scraping asynchronous

--- app/backend/scrapeNYTimes.js
+++ app/backend/scrapeNYTimes.js
@@ -6,41 +6,86 @@ const scrapeCategory = async (category) => {
     const html = await axios.get(url);
     const $ = await cheerio.load(html.data);
 
-    let articles = [];
+    // extract article elements
+    const elements = $("li.css-18yolpw");
     
-    $("li.css-18yolpw").each((i, element) => {
-        articles.push({
+    // asynchronously scrape each article
+    let promises = [];
+    for (let i = 0; i < elements.length; i++) {
+        promises.push(scrapeArticle(elements[i], $));
+    }
+
+    const articles = await Promise.all(promises);
+    return articles.filter(article => article !== null);
+}
+
+const scrapeArticle = async (element, $) => {
+    try {
+        // extract article title, sub-title, and url
+        let articleData = {
             title: $(element).find("h3").text(),
+            article: "",
             summary: $(element).find("p").text(),
             url: $(element).find("a").attr("href"),
             source: "New York Times"
-        })
-    });
+        };
 
-    return articles;
+        // update article field (commented out because it is not working yet)
+        // const html = await axios.get(`https://www.nytimes.com${articleData.url}`);
+        // const $$ = await cheerio.load(html.data);
+
+        // let articleText = [];
+        // $$(element).find("p.css-at9mc1").each((i, element) => {
+        //         articleText.push($$(element).text());
+        // });
+
+        // articleData.article = articleText.join(" ");
+        return articleData;
+    } catch (err) {
+        return null;
+    }
 }
 
 const scrapeNYTimes = async () => {
+    let promises = [
+        scrapeCategory("world"),
+        scrapeCategory("us"),
+        scrapeCategory("politics"),
+        scrapeCategory("science"),
+        scrapeCategory("sports"),
+        scrapeCategory("health"),
+        scrapeCategory("nyregion"),
+        scrapeCategory("business"),
+        scrapeCategory("opinion"),
+        scrapeCategory("arts"),
+        scrapeCategory("books"),
+        scrapeCategory("style"),
+        scrapeCategory("food"),
+        scrapeCategory("travel")
+    ];
+
+    let articles = await Promise.all(promises);
+
     let data = {
-        world: await scrapeCategory("world"),
-        us: await scrapeCategory("us"),
-        politics: await scrapeCategory("politics"),
-        science: await scrapeCategory("science"),
-        sports: await scrapeCategory("sports"),
-        health: await scrapeCategory("health"),
-        // ny: [],
-        // business: [],
-        // opinion: [],
-        // arts: [],
-        // books: [],
-        // style: [],
-        // food: [],
-        // travel: [],
-        // magazine: [],
-        // realEstate: [],
+        world: articles[0],
+        us: articles[1],
+        politics: articles[2],
+        science: articles[3],
+        sports: articles[4],
+        health: articles[5],
+        ny: articles[6],
+        business: articles[7],
+        opinion: articles[8],
+        arts: articles[9],
+        books: articles[10],
+        style: articles[11],
+        food: articles[12],
+        travel: articles[13],
     };
 
     return data;
 }
+
+console.log(await scrapeNYTimes());
 
 export default scrapeNYTimes;
--- app/backend/scrapeReuters.js
+++ app/backend/scrapeReuters.js
@@ -6,9 +6,10 @@ const scrapeCategory = async (category) => {
     const html = await axios.get(url);
     const $ = await cheerio.load(html.data);
 
-    // scrape article titles and urls
+    // extract article elements
     const elements = $("li.story-collection__story__LeZ29");
 
+    // asynchronously scrape each article
     let promises = [];
     for (let i = 0; i < elements.length; i++) {
         promises.push(scrapeArticle(elements[i], $));
@@ -23,6 +24,7 @@ const scrapeArticle = async (element, $) => {
         // extract article title and url
         let articleData = {
             title: $(element).find("h3").find("a").text(),
+            article: "",
             summary: "",
             url: $(element).find("h3").find("a").attr("href"),
             source: "Reuters"
@@ -33,13 +35,11 @@ const scrapeArticle = async (element, $) => {
         const $$ = await cheerio.load(html.data);
 
         let articleText = [];
-
         $$("p.text__text__1FZLe").each((i, element) => {
             articleText.push($$(element).text());
         });
 
-        articleData.summary = articleText.join(" ");
-        
+        articleData.article = articleText.join(" ");
         return articleData;
     } catch (err) {
         return null;
